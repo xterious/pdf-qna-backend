0022881fe527c59361fe34ff0eed0e0bb18af000
Kamalesh <kamalesh2023@gmail.com>
2025-04-26 00:15:40 +0530

    throw and exceptions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -19,5 +19,7 @@ def create_item(item: str):
 
 @app.get("/items/{item_id}")
 def get_item(item_id: int) -> str:
-    item = items[item_id]
-    return item
+    if(item_id >= len(items)):
+        raise HTTPException(status_code=404, detail="Item not found")
+    else:
+        return items[item_id]
