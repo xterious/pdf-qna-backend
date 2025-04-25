f241066833e5f3b7f3409d20cd32d5139297747a
Kamalesh <kamalesh2023@gmail.com>
2025-04-26 00:33:25 +0530

    json params

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str = None
+    is_done: bool = False
 
 items = []
 
@@ -11,14 +15,14 @@ def root():
 
 
 @app.post("/items")
-def create_item(item: str):
+def create_item(item: Item):
     items.append(item)
     return items
 
 
 
 @app.get("/items/{item_id}")
-def get_item(item_id: int) -> str:
+def get_item(item_id: int) -> Item:
     if(item_id >= len(items)):
         raise HTTPException(status_code=404, detail="Item not found")
     else:
