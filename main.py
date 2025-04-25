4c311429213fd46987a7d0809b7e6cf87cb35bc1
Kamalesh <kamalesh2023@gmail.com>
2025-04-26 00:13:45 +0530

    basic get post requests

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+items = []
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return items
+
+
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int) -> str:
+    item = items[item_id]
+    return item
